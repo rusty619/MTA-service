87797f75d9619c31335bff62da4ad48f1c490d21
Ruslan Khait <rusty6916@gmail.com>
2022-04-01 17:19:47 -0400

    click event

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ const getAllStations = async() => {
         //console.log('Station', element)
         let h2 = document.createElement('h2')
         h2.innerText = element.name
+        h2.dataset.stationId = element.id
+        h2.addEventListener('click', () => {
+            getArrivalTime(h2.dataset.stationId)
+        })
         div.append(h2)
     })
+}
+
+const getArrivalTime = async (stationId) => {
+    let req = await fetch(`https://mtaapi.herokuapp.com/api?id=${stationId}`)
+    let res = await req.json()
+    console.log("Arrival Time",res.result.arrivals)
 }
 
 getAllStations()
